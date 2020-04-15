836825f0838dbd784dd5801fe2624362433c0415
JyotiKumarPoddar <jyoti@skenariolabs.com>
2020-04-15 12:32:53 +0300

    footer added

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <div className="d-flex flex-column">
+            <footer className="py-4 bg-light bg-secondary">
+                <div className="container text-center text-primary">
+                    <small>Copyright &copy; Your Website</small>
+                </div>
+            </footer>
+        </div>
+    );
+}
+
+export default Footer;
