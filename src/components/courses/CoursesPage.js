9e7dfec63b1ec262995646a2280300475c4b65dd
JyotiKumarPoddar <jyoti@skenariolabs.com>
2020-04-16 11:22:42 +0300

    added simple form

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,8 +1,32 @@
 import React from "react";
 
 class CoursesPage extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      course: {
+        title: ''
+      }
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange = event => {
+    const course = { ...this.state.course, title: event.target.value };
+    this.setState({ course: course });
+    //or  this.setState({ course });
+  }
+
   render() {
-    return <h2>Courses</h2>;
+    return (
+      <form>
+        <h2>Course</h2>
+        <h3>Add courses</h3>
+        <input type='text' onChange={this.handleChange}
+          value={this.state.course.title} />
+        <input className='btn btn-primary' type='submit' value="Save" />
+      </form>
+    );
   }
 }
 
